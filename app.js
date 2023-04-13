a697dd7597b98f67254d5519d4e3764f63983659
Saransh Raina <saranshraina1@gmail.com>
2023-04-12 23:12:14 -0500

    Prep for 1.1

--- app.js
+++ app.js
@@ -8,14 +8,16 @@ const Hello = (props) => {
 }
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const friends = [
+    { name: 'Peter', age: 4 },
+    { name: 'Maya', age: 10 },
+  ]
+
   return (
-    <>
-      <hi> Greetings </hi>
-      <Hello name='Maya' age={26 + 10}/>
-      <Hello name={name} age={age} />
-    </>
+    <div>
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
+    </div>
   )
 }
 
