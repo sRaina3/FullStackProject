6590d4bf3e13cbb4e332aef116cfd41a67a8a21f
Saransh Raina <saranshraina1@gmail.com>
2023-04-23 19:42:08 -0500

    2.3 done

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -5,7 +5,7 @@ const Content = ({parts}) => {
   return (
     <div>
       {parts.map(part => <Part key={part.id} name={part.name} ex={part.exercises}/>)}
-      <Total parts={parts}/>
+      <Total parts={parts.map(part => part.exercises)}/>
     </div>
   )
 }
--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -1,8 +1,5 @@
 const Total = ({parts}) => {
-  let total = 0;
-  for (let i = 0; i < parts.length; i++) {
-    total += parts[i].exercises
-  }
+  let total = parts.reduce((tot, cur) => tot + cur)
   return (
     <b> total of {total} exercises </b>
   )
