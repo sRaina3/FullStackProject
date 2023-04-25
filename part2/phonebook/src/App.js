314e9f180eee0916720e96af66525b498fbc1af2
Saransh Raina <saranshraina1@gmail.com>
2023-04-24 19:27:12 -0500

    2.7 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,40 @@
 import { useState } from 'react'
+import Note from './components/Note'
 
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
-  const [newName, setNewName] = useState('')
+  const [newName, setNewName] = useState('enter name')
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const objectToAdd = {
+      name: newName
+    }
+    setPersons(persons.concat(objectToAdd))
+    setNewName('')
+  }
+
+  const updateName = (event) => {
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addNote}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={updateName}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        {persons.map(p => <Note key={p.name} name={p.name} />)}
+      </div>
     </div>
   )
 }
---
+++ part2/phonebook/src/components/Note.js
@@ -0,0 +1,5 @@
+const Note = ({name}) => {
+  return <div>{name}</div>
+}
+
+export default Note
