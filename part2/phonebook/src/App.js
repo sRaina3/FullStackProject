395c0e00bdb4a5eb30292e78389ab2cc11b7b7b3
Saransh Raina <saranshraina1@gmail.com>
2023-04-25 22:07:42 -0500

    2.11 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', num: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', num: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', num: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', num: '39-23-6423122', id: 4 }
-  ]) 
-
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('enter name')
   const [newNum, setNewNum] = useState('enter number')
   const [filter, setFilter] = useState('')
   const [displayFilter, setDisplay] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   const addNote = (event) => {
     event.preventDefault()
@@ -23,7 +28,7 @@ const App = () => {
     } else {
       const objectToAdd = {
         name: newName,
-        num: newNum
+        number: newNum
       }
       setPersons(persons.concat(objectToAdd))
       setNewName('')
--- part2/phonebook/src/components/Persons.js
+++ part2/phonebook/src/components/Persons.js
@@ -1,6 +1,6 @@
 import Note from './Note'
 
 const Persons = ({arr}) =>
-    <div> {arr.map(p => <Note key={p.name} name={p.name} num={p.num}/>)} </div>
+    <div> {arr.map(p => <Note key={p.name} name={p.name} num={p.number}/>)} </div>
 
 export default Persons
