e9bc6779805eaa0480393ab87b0044fb6a9b87bc
Saransh Raina <saranshraina1@gmail.com>
2023-05-04 19:43:10 -0500

    css error display

--- part2/course/src/App.js
+++ part2/course/src/App.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react'
 import Note from './components/Note'
+import Notif from './components/Notification'
 import noteService from './services/Notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   useEffect(() => {
     noteService
@@ -41,9 +43,12 @@ const App = () => {
       setNotes(notes.map(note => note.id !== id ? note : returnedNote))
     })
     .catch(error => {
-      alert(
-        `the note '${note.content}' was already deleted from server`
+      setErrorMessage(
+        `Note '${note.content}' was already removed from server`
       )
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
       setNotes(notes.filter(n => n.id !== id))
     })
   }
@@ -56,6 +61,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notif message={errorMessage}/>
       <div>
         <button onClick={() => {setShowAll(!showAll)}}>show {showAll ? 'important' : 'all'}</button>
       </div>
