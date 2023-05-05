1543a9d081c234f25ad148a5749d6cfcdb836494
Saransh Raina <saranshraina1@gmail.com>
2023-05-05 14:00:07 -0500

    Countries are being retrived

--- part2/country-data/src/App.js
+++ part2/country-data/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
-function App() {
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [displayCountries, setDisplayCountries] = useState([])
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        console.log('Got response')
+        setCountries(response.data)
+      })
+  }, [])
+
+  const updateDisplay = () => {
+    setDisplayCountries(countries.filter(c => c.name.includes(filter)))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit>
+        <div>
+          <div>find countries <input></input></div>
+        </div>
+      </form>
+      {countries.map(c => <div>{c.name.common}</div>)}
     </div>
-  );
+  )
 }
 
 export default App;
