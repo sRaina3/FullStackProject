1d6664458080ae394923687e8d1959f745950c8e
Saransh Raina <saranshraina1@gmail.com>
2023-05-05 14:37:16 -0500

    Displaying multiple correctly

--- part2/country-data/src/components/Countries.js
+++ part2/country-data/src/components/Countries.js
@@ -1,9 +1,19 @@
 const Countries = ({countries}) => {
-  return (
-    <div>
-      {countries.map(c => <div>{c.name.common}</div>)}
-    </div>
-  )
+  if (countries.length > 10) {
+    return (
+      <div>
+        Too many matches, specify another filter
+      </div>
+    )
+  } else if (countries.length !== 0) {
+
+  } else {
+    return (
+      <div>
+        {countries.map(c => <div key={c.name.common}> {c.name.common} </div>)}
+      </div>
+    )
+  }
 }
 
 export default Countries
