57ad8522cc271439ef8111429915b5d23067deb3
Saransh Raina <saranshraina1@gmail.com>
2023-05-05 19:49:00 -0500

    2.19 done

--- part2/country-data/src/components/Countries.js
+++ part2/country-data/src/components/Countries.js
@@ -1,6 +1,14 @@
 import Country from './Country'
+import { useState } from 'react'
 
 const Countries = ({countries}) => {
+  const [shownCountries, setShownCountries] = useState([])
+  const displayCountry = (c) => {
+    if (!shownCountries.includes(c)) {
+      setShownCountries(shownCountries.concat(c).filter(c => countries.includes(c)))
+    }
+  }
+
   if (countries.length > 10) {
     return (
       <div>
@@ -9,10 +17,8 @@ const Countries = ({countries}) => {
     )
   } else if (countries.length < 3 && countries.length !== 0) {
     const country1 = countries[0]
-    const langArr1 = Object.values(country1.languages)
     if (countries.length > 1) {
       const country2 = countries[1]
-      const langArr2 = Object.values(country2.languages)
       return (
         <div>
           <Country c={country1}/>
@@ -29,7 +35,11 @@ const Countries = ({countries}) => {
   } else {
     return (
       <div>
-        {countries.map(c => <div key={c.name.common}> {c.name.common} </div>)}
+        {countries.map(c => 
+          <div key={c.name.common}> 
+            {c.name.common} <button onClick={() => {displayCountry(c)}}>show</button> 
+          </div>)}
+        <div> {shownCountries.map(elem => <div key={elem.cca2}> <Country c={elem}/> </div>)} </div>
       </div>
     )
   }
