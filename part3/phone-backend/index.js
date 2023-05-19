cfc286947999df0ff5b6f389eea80b792f7af54f
Saransh Raina <saranshraina1@gmail.com>
2023-05-19 18:57:24 -0500

    3.8 done

--- part3/phone-backend/index.js
+++ part3/phone-backend/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+morgan.token('body', function(req, res) {
+  if (req.method === 'POST') {
+  return JSON.stringify(req.body);
+  }
+});
 
 let nums = [
   { 
