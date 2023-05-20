441c5de88bc6f66e1fb8f822a58ab188478a844d
Saransh Raina <saranshraina1@gmail.com>
2023-05-19 19:58:44 -0500

    changes

--- part2/course/src/services/notes.js
+++ part2/course/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = 'https://course-render-test.onrender.com/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
